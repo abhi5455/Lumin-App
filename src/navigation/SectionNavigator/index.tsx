75998452d68cc7064fe1441ce3c09e001186b1dd
Abhiram Ashok <abhiramashok1062004@gmail.com>
2025-11-06 13:51:29 +0530

    Profile

--- src/navigation/SectionNavigator/index.tsx
+++ src/navigation/SectionNavigator/index.tsx
@@ -1,6 +1,4 @@
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import WelcomeScreen from "../../screens/OnboardingScreens/WelcomeScreen.tsx";
-import RegisterScreen from "../../screens/OnboardingScreens/RegisterScreen.tsx";
 import AlumniDetailsScreen from "../../screens/AlumniScreen/AlumniDetailsScreen";
 import CompanyDetailsScreen from "../../screens/CompaniesScreen/CompanyDetailsScreen";
 import AddResourceScreen from "../../screens/ResourceScreen/AddResourceScreen";
@@ -11,18 +9,6 @@ export default function SectionNavigator() {
     const Stack = createNativeStackNavigator()
     return (
         <Stack.Navigator>
-            <Stack.Screen
-                name="WelcomeScreen"
-                options={{headerShown: false, gestureEnabled: false}}
-                component={WelcomeScreen}
-            />
-            <Stack.Screen
-                name="RegisterScreen"
-                options={{headerShown: false, gestureEnabled: false}}
-                component={RegisterScreen}
-            />
-
-
             <Stack.Screen
                 name="AlumniDetailsScreen"
                 options={{headerShown: false, gestureEnabled: false}}
