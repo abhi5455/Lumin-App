380fddff92a7c38aebb4d53c574315229e441740
Abhiram Ashok <abhiramashok1062004@gmail.com>
2026-01-17 12:58:50 +0530

    README.md

--- src/screens/AlumniScreen/AlumniDetailsScreen/About.tsx
+++ src/screens/AlumniScreen/AlumniDetailsScreen/About.tsx
@@ -6,17 +6,14 @@ export default function About({alumnus}: { alumnus: IStudent }) {
     return (
         <>
             <View
-                className="flex flex-col justify-center border-[1px] bg-gray-100/35 border-[#006a63]/50 rounded-xl p-2 mx-5 mt-5">
-                <View className="flex flex-row items-center gap-3">
-                    <View
-                        className="flex justify-center items-center self-start mt-2 ml-2 bg-primary/10 w-12 h-12 rounded-full">
-                        <User size={19} color={"#006a63"}/>
+                className="flex flex-col justify-center gap-1.5 border-[1px] bg-gray-100/35 border-[#006a63]/50 rounded-xl p-2 mx-5 mt-5">
+                <View className="flex flex-row items-start w-full gap-4 mt-2">
+                    <View className="flex justify-center items-center self-start ml-2 bg-primary/10 w-[45px] h-[45px] rounded-full">
+                        <User size={19} color={"#006a63"} />
                     </View>
-
-                    <View className="flex-1">
-                        <Text className="font-poppinsMedium text-lg">
-                            About
-                        </Text>
+                    <View className="">
+                        <Text className="font-poppinsMedium text-lg">About</Text>
+                        <Text className="font-poppinsLight text-gray-600 text-[13px]">Profile overview and interests</Text>
                     </View>
                 </View>
 
